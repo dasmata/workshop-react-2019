388ca7d4d10a2bf7873f5996d0b169fe2c94e32c
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-21 16:54:17 +0200

    added static plungin for babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,11 @@ const config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                    },
+
                 }
             }
         ]
