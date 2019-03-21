b1c2acf5b21b13ba67548bec57633862467c63e8
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-21 16:48:33 +0200

    controlled components

---
+++ src/js/controlled-components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RadioGroup from "./RadioGroup";
+
+const App = () => (
+    <RadioGroup>
+        <RadioGroup.Option value={"First"} label={"1st"} />
+        <RadioGroup.Option value={"Second"} />
+        <RadioGroup.Option value={"Third"} />
+    </RadioGroup>
+);
+
+
+export default App;
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from "./components/HelloWorld";
-
-
-const App = (props) => {
-    return <HelloWorld />;
-};
+import App from "./controlled-components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
