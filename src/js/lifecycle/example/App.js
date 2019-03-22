fce55dda74635cec91dc5bde64edf4d8b49148f0
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-22 14:57:36 +0200

    added lifecycle skeleton

---
+++ src/js/lifecycle/example/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+class App extends React.Component {
+
+    static getDerivedStateFromProps(props, state){
+
+    }
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    shouldComponentUpdate(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    render(){
+        return <Loader />;
+    }
+}
