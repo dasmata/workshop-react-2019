d61f41b3ce67184920931991623f46b856556073
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-22 05:34:50 +0200

    implemented App as a state component

--- src/js/controlled-components/App.js
+++ src/js/controlled-components/App.js
@@ -1,14 +1,27 @@
-import React from "react";
-import RadioGroup from "./RadioGroup";
+import React from 'react';
+import RadioGroup from './RadioGroup';
 
-const App = () => (
-    <RadioGroup>
-        <h5>Value: {this.state.value}</h5>
-        <RadioGroup.Option value={"First"} label={"1st"} />
-        <RadioGroup.Option value={"Second"} />
-        <RadioGroup.Option value={"Third"} />
-    </RadioGroup>
-);
+class App extends React.Component {
 
+    state = {
+        value: "Second"
+    };
+
+    onChange = value => {
+        this.setState({ value });
+    };
+
+    render () {
+        return (
+            <RadioGroup onChange={this.onChange} value={this.state.value}>
+                <h5>Value: {this.state.value}</h5>
+                <RadioGroup.Option value={'First'} label={'1st'}/>
+                <RadioGroup.Option value={'Second'}/>
+                <RadioGroup.Option value={'Third'}/>
+            </RadioGroup>
+        );
+    }
+
+}
 
 export default App;
