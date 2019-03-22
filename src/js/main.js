7b45261dfe226e14ae634874e9e18e53bf2cf32b
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-22 14:40:36 +0200

    changed the app to the example

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./state/solution/App";
+import App from "./state/example/App";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
