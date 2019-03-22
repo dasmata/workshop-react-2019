a7ce2c1eea34ef9f4ab564a38fb6fd4d15936674
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2019-03-22 06:28:31 +0200

    added class properties syntax

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ const config = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react'],
-
                         plugins: ["@babel/plugin-proposal-class-properties"],
                     },
 
